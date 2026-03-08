2f5281136d0adb3ab28863a6ce5c8198187a495a
AtharvaZalkikar <atharvazalkikar@gmail.com>
2026-03-08 14:01:21 +0000

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel #pydantic models
 
 app = FastAPI()
+
+shipments = [] #temporary storage/database
+
+# ShipmentCreate → request schema
+class ShipmentCreate(BaseModel):
+    destination: str
+    status: str
+
+'''
+What response_model Does:
+
+1 Validate response data
+2 Filter unwanted fields
+3 Improve Swagger docs
+4 Generate OpenAPI schema
+'''
+# ShipmentResponse → response schema
+class ShipmentResponse(BaseModel):   #in production we want to control the response structure
+    id: int
+    destination: str
+    status: str
+
 
 @app.get("/")
 def root():
@@ -9,8 +32,6 @@ def root():
 @app.get("/health")
 def health_check():
     return {"status" : "healthy"}
-
-shipments = [] #temporary storage/database
 
 @app.get("/shipments")
 def get_shipments():
@@ -23,12 +44,15 @@ def get_shipments(shipment_id: int):
             return shipment
     return {"error": "Shipment not found"}
 
-@app.post("/shipments")
-def create_shipment(shipment:dict):
+@app.post("/shipments", response_model=ShipmentResponse)
+def create_shipment(shipment: ShipmentCreate):
     shipment_id = len(shipments) + 1
+
+    shipment_data = shipment.model_dump()  # convert Pydantic → dict
     shipment["id"] = shipment_id
-    shipments.append(shipment)
-    return shipment   
+
+    shipments.append(shipment_data)
+    return shipment_data   
 
 @app.delete("/shipments/{shipment_id}")
 def delete_shipment(shipment_id: int):
