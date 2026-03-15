40d86e793e6116681712fbd4dbaca8c75822c7b2
AtharvaZalkikar <atharvazalkikar@gmail.com>
2026-03-15 14:07:05 +0000

    Add Indexing to Shipment db table for faster searches on million rows

--- app/models/shipment.py
+++ app/models/shipment.py
@@ -1,13 +1,28 @@
 from sqlmodel import SQLModel, Field
 from app.models.shipment_status import ShipmentStatus
 from datetime import datetime, timezone
+from sqlalchemy import Index
+
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Index
+from app.models.shipment_status import ShipmentStatus
+from datetime import datetime, timezone
+
 
 class Shipment(SQLModel, table=True):
 
-    id: int | None = Field(default=None, primary_key=True)
-    destination: str
-    status: ShipmentStatus = Field(default=ShipmentStatus.CREATED) #updated from str to use ENUMS in shipment_status.py
+    __table_args__ = (
+        Index("idx_shipment_destination", "destination"),
+        Index("idx_shipment_status", "status"),
+        Index("idx_shipment_created_at", "created_at"),
+    )
 
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))  #default_factory generates a new timestamp for every row.
+    id: int | None = Field(default=None, primary_key=True)
+
+    destination: str
+
+    status: ShipmentStatus = Field(default=ShipmentStatus.CREATED)
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     updated_at: datetime | None = Field(default=None)
